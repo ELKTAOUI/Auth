ded7994cf3f3d52e9e85d8a735ea51229e896234
EL_KTAOUI Zakaria <elktaoui@outlook.com>
2013-11-04 01:04:37 +0000

    Create hacker.php

---
+++ hacker.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+$_SESSION['Auth']=array(
+    'login'=>'test',
+    'pass'=>'jdjlcndl'
+);
+?>
+<html>
+    <body>
+        <h1>Page Public(Hacker)</h1>
+    </body>
+</html>
